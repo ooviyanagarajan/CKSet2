ee0f592168b877c089fbc8c7c817160908afb548
ooviyanagarajan <46079755+ooviyanagarajan@users.noreply.github.com>
2019-08-17 22:50:46 +0530

    Update Amstrong.java

--- Amstrong.java
+++ Amstrong.java
@@ -1,13 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package CodeKata_Set2;
 
-/**
- *
- * @author crm systems
- */
 import java.util.*;
 public class Amstrong {
     public static void main(String[]args){
@@ -40,31 +31,4 @@ public class Amstrong {
         
     }
 }
-  /*   public static void main(String[] args) {
-        int number , originalNumber, remainder;
-        int digits = 0;
-            int result = 0;
-         Scanner sc=new Scanner(System.in);
-         System.out.println("Enter n:");
-        number=sc.nextInt();
-          originalNumber = number;
-         while (originalNumber != 0) {
-                originalNumber /= 10;
-                ++digits;
-            }
-            originalNumber = number;
-            // result contains sum of nth power of its digits
-            while (originalNumber != 0) {
-                 remainder = originalNumber % 10;
-                result += Math.pow(remainder, digits);
-                originalNumber /= 10;
-            }
-
-        if(result == number)
-            System.out.println(number + " is an Armstrong number.");
-        else
-            System.out.println(number + " is not an Armstrong number.");
-    }
-
-}
-*/
+  
